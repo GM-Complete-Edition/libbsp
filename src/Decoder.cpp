d1f21121e56151cf6e6e9be34d84b20eee20f972
Dorian <villainkernelerror@gmail.com>
2022-07-22 19:21:22 +0200

    Add decoding functions for int8_t, int16_t, int64_t, uint64_t, double

--- src/Decoder.cpp
+++ src/Decoder.cpp
@@ -4,8 +4,23 @@
 #include "libbsp/Decoder.hpp"
 
 template<>
+int8_t BSPDecoder::read(MemoryStream& stream) {
+    return std::bit_cast<int8_t>(stream.readByte());
+}
+
+template<>
 uint8_t BSPDecoder::read(MemoryStream& stream) {
     return stream.readByte();
+}
+
+template<>
+int16_t BSPDecoder::read(MemoryStream& stream) {
+    uint8_t secondByte = stream.readByte();
+    uint8_t firstByte = stream.readByte();
+
+    return std::bit_cast<int16_t>(
+        static_cast<uint16_t>((static_cast<uint16_t>(firstByte) << 8) | static_cast<uint16_t>(secondByte))
+    );
 }
 
 template<>
@@ -13,7 +28,7 @@ uint16_t BSPDecoder::read(MemoryStream& stream) {
     uint8_t secondByte = stream.readByte();
     uint8_t firstByte = stream.readByte();
 
-    return (static_cast<uint16_t>(firstByte) << 8) | static_cast<uint16_t>(secondByte);
+    return static_cast<uint16_t>((static_cast<uint16_t>(firstByte) << 8) | static_cast<uint16_t>(secondByte));
 }
 
 template<>
@@ -45,6 +60,48 @@ uint32_t BSPDecoder::read(MemoryStream& stream) {
 }
 
 template<>
+int64_t BSPDecoder::read(MemoryStream& stream) {
+    uint8_t eightByte = stream.readByte();
+    uint8_t seventhByte = stream.readByte();
+    uint8_t sixthByte = stream.readByte();
+    uint8_t fifthByte = stream.readByte();
+    uint8_t fourthByte = stream.readByte();
+    uint8_t thirdByte = stream.readByte();
+    uint8_t secondByte = stream.readByte();
+    uint8_t firstByte = stream.readByte();
+
+    return std::bit_cast<int64_t>((static_cast<uint64_t>(firstByte) << 56) 
+        | (static_cast<uint64_t>(secondByte) << 48) 
+        | (static_cast<uint64_t>(thirdByte) << 40) 
+        | (static_cast<uint64_t>(fourthByte) << 32)
+        | (static_cast<uint64_t>(fifthByte) << 24)
+        | (static_cast<uint64_t>(sixthByte) << 16)
+        | (static_cast<uint64_t>(seventhByte) << 8)
+        | (static_cast<uint64_t>(eightByte) << 0));
+}
+
+template<>
+uint64_t BSPDecoder::read(MemoryStream& stream) {
+    uint8_t eightByte = stream.readByte();
+    uint8_t seventhByte = stream.readByte();
+    uint8_t sixthByte = stream.readByte();
+    uint8_t fifthByte = stream.readByte();
+    uint8_t fourthByte = stream.readByte();
+    uint8_t thirdByte = stream.readByte();
+    uint8_t secondByte = stream.readByte();
+    uint8_t firstByte = stream.readByte();
+
+    return (static_cast<uint64_t>(firstByte) << 56) 
+        | (static_cast<uint64_t>(secondByte) << 48) 
+        | (static_cast<uint64_t>(thirdByte) << 40) 
+        | (static_cast<uint64_t>(fourthByte) << 32)
+        | (static_cast<uint64_t>(fifthByte) << 24)
+        | (static_cast<uint64_t>(sixthByte) << 16)
+        | (static_cast<uint64_t>(seventhByte) << 8)
+        | (static_cast<uint64_t>(eightByte) << 0);
+}
+
+template<>
 float BSPDecoder::read(MemoryStream& stream) {
     uint8_t fourthByte = stream.readByte();
     uint8_t thirdByte = stream.readByte();
@@ -57,6 +114,27 @@ float BSPDecoder::read(MemoryStream& stream) {
         | (static_cast<uint32_t>(thirdByte) << 8) 
         | (static_cast<uint32_t>(fourthByte) << 0)
     );
+}
+
+template<>
+double BSPDecoder::read(MemoryStream& stream) {
+    uint8_t eightByte = stream.readByte();
+    uint8_t seventhByte = stream.readByte();
+    uint8_t sixthByte = stream.readByte();
+    uint8_t fifthByte = stream.readByte();
+    uint8_t fourthByte = stream.readByte();
+    uint8_t thirdByte = stream.readByte();
+    uint8_t secondByte = stream.readByte();
+    uint8_t firstByte = stream.readByte();
+
+    return std::bit_cast<double>((static_cast<uint64_t>(firstByte) << 56) 
+        | (static_cast<uint64_t>(secondByte) << 48) 
+        | (static_cast<uint64_t>(thirdByte) << 40) 
+        | (static_cast<uint64_t>(fourthByte) << 32)
+        | (static_cast<uint64_t>(fifthByte) << 24)
+        | (static_cast<uint64_t>(sixthByte) << 16)
+        | (static_cast<uint64_t>(seventhByte) << 8)
+        | (static_cast<uint64_t>(eightByte) << 0));
 }
 
 template<typename T>
