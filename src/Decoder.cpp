e2344024435fdb32fb7c84782b30895541f56da6
Dorian <villainkernelerror@gmail.com>
2022-07-22 19:16:11 +0200

    Rearrange definitions in Decoder.cpp to match order present in Decoder.hpp

--- src/Decoder.cpp
+++ src/Decoder.cpp
@@ -3,37 +3,6 @@
 
 #include "libbsp/Decoder.hpp"
 
-template<typename T>
-std::vector<T> BSPDecoder::readMany(MemoryStream& stream) {
-    auto size = read<int32_t>(stream);
-
-    std::vector<T> elements;
-
-    elements.reserve(size);
-
-    for (int32_t i = 0; i < size; i++) {
-        elements.push_back(read<T>(stream));
-    }
-
-    return elements;
-}
-
-template<typename T>
-std::optional<T> BSPDecoder::readOptional(MemoryStream& stream) {
-    auto isPresent = read<int32_t>(stream);
-
-    if (isPresent) {
-        return std::make_optional<T>(read<T>(stream));
-    } else {
-        return std::nullopt;
-    };
-}
-
-template<typename... Ts>
-std::tuple<Ts...> BSPDecoder::readTuple(MemoryStream& stream) {
-    return std::tuple<Ts...>((read<Ts>(stream))...);
-}
-
 template<>
 uint8_t BSPDecoder::read(MemoryStream& stream) {
     return stream.readByte();
@@ -88,4 +57,35 @@ float BSPDecoder::read(MemoryStream& stream) {
         | (static_cast<uint32_t>(thirdByte) << 8) 
         | (static_cast<uint32_t>(fourthByte) << 0)
     );
+}
+
+template<typename T>
+std::vector<T> BSPDecoder::readMany(MemoryStream& stream) {
+    auto size = read<int32_t>(stream);
+
+    std::vector<T> elements;
+
+    elements.reserve(size);
+
+    for (int32_t i = 0; i < size; i++) {
+        elements.push_back(read<T>(stream));
+    }
+
+    return elements;
+}
+
+template<typename T>
+std::optional<T> BSPDecoder::readOptional(MemoryStream& stream) {
+    auto isPresent = read<int32_t>(stream);
+
+    if (isPresent) {
+        return std::make_optional<T>(read<T>(stream));
+    } else {
+        return std::nullopt;
+    };
+}
+
+template<typename... Ts>
+std::tuple<Ts...> BSPDecoder::readTuple(MemoryStream& stream) {
+    return std::tuple<Ts...>((read<Ts>(stream))...);
 }
