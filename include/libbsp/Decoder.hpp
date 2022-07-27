a0af011ef7ae7620fe0da8d4a0a61761a71c9f15
Dorian <villainkernelerror@gmail.com>
2022-07-27 19:58:12 +0200

    Move declarations of generic readMany, readString, readEnum, readOptional and readTuple to header file, make some of them inline

--- include/libbsp/Decoder.hpp
+++ include/libbsp/Decoder.hpp
@@ -20,19 +20,65 @@ public:
     [[nodiscard]] T read(MemoryStream& stream, PARAMETERS... parameters);
 
     template<typename T>
-    [[nodiscard]] std::vector<T> readMany(MemoryStream& stream);
+    [[nodiscard]] std::vector<T> readMany(MemoryStream& stream) {
+        auto size = read<int32_t>(stream);
+
+        std::vector<T> elements;
+
+        elements.reserve(size);
+
+        for (int32_t i = 0; i < size; i++) {
+            elements.push_back(read<T>(stream));
+        }
+
+        return elements;
+    }
 
     template<typename T> requires std::is_same_v<T, uint8_t> || std::is_same_v<T, int32_t>
-    [[nodiscard]] std::string readString(MemoryStream& stream, bool isNullTerminated = false);
+    [[nodiscard]] std::string readString(MemoryStream& stream, bool isNullTerminated = false) {
+        auto length = read<int32_t>(stream);
+
+        if (length > 0) {
+            if (isNullTerminated) {
+                length -= 1;
+            }
+
+            char* const characters = new char[length + 1];
+
+            for (int i = 0; i < length; i++) {
+                characters[i] = static_cast<char>(read<T>(stream));
+            }
+
+            characters[length] = 0;
+
+            std::string result = characters;
+
+            return result;
+        } else {
+            return std::string();
+        }
+    }
 
     template<typename T> requires std::is_enum_v<T>
-    [[nodiscard]] T readEnum(MemoryStream& stream);
+    [[nodiscard]] inline T readEnum(MemoryStream& stream) {
+        return static_cast<T>(read<std::underlying_type_t<T>>(stream));
+    }
 
     template<typename T>
-    [[nodiscard]] std::optional<T> readOptional(MemoryStream& stream);
+    [[nodiscard]] std::optional<T> readOptional(MemoryStream& stream) {
+        auto isPresent = read<int32_t>(stream);
+
+        if (isPresent) {
+            return std::make_optional<T>(read<T>(stream));
+        } else {
+            return std::nullopt;
+        };
+    }
 
     template<typename... Ts>
-    [[nodiscard]] std::tuple<Ts...> readTuple(MemoryStream& stream);
+    [[nodiscard]] inline std::tuple<Ts...> readTuple(MemoryStream& stream) {
+        return std::tuple<Ts...>((read<Ts>(stream))...);
+    }
 };
 
 }
